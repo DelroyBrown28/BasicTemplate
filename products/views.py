98bd315136d9d284ba37e6d4dd31a4e21d4d43de
Delroy Brown <delroybrown.db@gmail.com>
2021-05-17 11:08:49 +0100

    Product details page created

--- products/views.py
+++ products/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
+
+
+
 
 def all_products(request):
     """Shows and displays all products."""
-    
     products = Product.objects.all()
     
     product_context = {
@@ -11,3 +13,16 @@ def all_products(request):
     }
     
     return render(request, 'products/products.html', product_context)
+
+
+
+def product_detail(request, product_id):
+    """Displays individual products"""
+    product = get_object_or_404(Product, pk=product_id)
+    
+    product_context = {
+        'product' : product,
+    }
+    
+    return render(request, 'products/product_detail.html', product_context)
+
