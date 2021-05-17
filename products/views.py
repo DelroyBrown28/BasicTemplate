29a60840961e537f072a387c74ce1219387020a9
Delroy Brown <delroybrown.db@gmail.com>
2021-05-17 12:03:08 +0100

    Search query set up

--- products/views.py
+++ products/views.py
@@ -1,15 +1,29 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
-
 
 
 
 def all_products(request):
     """Shows and displays all products."""
     products = Product.objects.all()
+    query = None
+    
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, 'Please enter a search query!')
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+            
     
     product_context = {
         'products' : products,
+        'search_term' : query,
     }
     
     return render(request, 'products/products.html', product_context)
@@ -17,7 +31,7 @@ def all_products(request):
 
 
 def product_detail(request, product_id):
-    """Displays individual products"""
+    """Displays individual products."""
     product = get_object_or_404(Product, pk=product_id)
     
     product_context = {
