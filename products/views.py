055aa19e4c11707e54c9d6d3c66c85dc555b169e
Delroy Brown <delroybrown.db@gmail.com>
2021-05-17 12:46:15 +0100

    Can now sort products based on price and rating etc

--- products/views.py
+++ products/views.py
@@ -10,15 +10,32 @@ def all_products(request):
     products = Product.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
     
     if request.GET:
         
+        # Handles sorting by price, rating etc...
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+                
+        # Handles categories        
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
         
-        
+        # Handles Searches
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -29,10 +46,13 @@ def all_products(request):
             products = products.filter(queries)
             
     
+    current_sorting = f'{sort}_{direction}'
+    
     product_context = {
         'products' : products,
         'search_term' : query,
         'current_categories' : categories,
+        'current_sorting' : current_sorting,
     }
     
     return render(request, 'products/products.html', product_context)
