9fdd8cf14c10c86ac8218e96855c7d7f28f82d45
Delroy Brown <delroybrown.db@gmail.com>
2021-06-06 11:20:26 +0100

    Added some security to editing and delting products

--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
@@ -70,8 +71,13 @@ def product_detail(request, product_id):
     
     return render(request, 'products/product_detail.html', product_context)
 
+@login_required
 def add_product(request):
     """To quickly add a product to the store"""
+    if not request.user.is_superuser:
+        messages.error(request, 'Nope! Only the store owner can do that')
+        return redirect(reverse('home'))
+    
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
@@ -90,9 +96,14 @@ def add_product(request):
     }
     
     return render(request, template, context)
-      
+
+@login_required      
 def edit_product(request, product_id):
     """Edit product in store"""
+    if not request.user.is_superuser:
+        messages.error(request, 'Nope! Only the store owner can do that')
+        return redirect(reverse('home'))
+    
     product = get_object_or_404(Product, pk=product_id)
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES, instance=product)
@@ -113,8 +124,8 @@ def edit_product(request, product_id):
     }
     
     return render(request, template, context)   
- 
-    
+
+@login_required    
 def delete_product(request, product_id):
     """ Delete store product """
     if not request.user.is_superuser:
