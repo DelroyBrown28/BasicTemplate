3c01bb254e0e903cfc410e60dff8a6c3b09a5484
Delroy Brown <delroybrown.db@gmail.com>
2021-06-06 10:50:42 +0100

    all CRUD operations working with products

--- products/views.py
+++ products/views.py
@@ -75,9 +75,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product! Please check all details and try again.')
     else:
@@ -90,8 +90,7 @@ def add_product(request):
     }
     
     return render(request, template, context)
-    
-    
+      
 def edit_product(request, product_id):
     """Edit product in store"""
     product = get_object_or_404(Product, pk=product_id)
@@ -113,5 +112,16 @@ def edit_product(request, product_id):
         'product' : product,
     }
     
-    return render(request, template, context)    
+    return render(request, template, context)   
+ 
     
+def delete_product(request, product_id):
+    """ Delete store product """
+    if not request.user.is_superuser:
+        messages.error(request, 'Nope! Only the store owner can do that.')
+        return redirect(reverse('home'))
+
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product Deleted')
+    return redirect(reverse('products'))
