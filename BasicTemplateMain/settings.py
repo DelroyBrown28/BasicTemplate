cf851089cbd516d352bfb40f41139523c2f6923b
Delroy Brown <delroybrown.db@gmail.com>
2021-06-07 12:42:34 +0100

    Preparing Deploy

--- BasicTemplateMain/settings.py
+++ BasicTemplateMain/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['nueui-basictemplate.herokuapp.com', 'localhost']
 
 
 
@@ -107,12 +107,20 @@ WSGI_APPLICATION = 'BasicTemplateMain.wsgi.application'
 
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
+
+
 
 
 
