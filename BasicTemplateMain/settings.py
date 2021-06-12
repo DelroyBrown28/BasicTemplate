b105b557852939dcadfc735a171914e8be2113a3
Delroy Brown <delroybrown.db@gmail.com>
2021-06-12 14:29:00 +0100

    Added email settings

--- BasicTemplateMain/settings.py
+++ BasicTemplateMain/settings.py
@@ -198,4 +198,14 @@ STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
 STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET')
 STRIPE_ENDPOINT_SECRET = os.environ.get('STRIPE_ENDPOINT_SECRET')
 
-DEFAULT_FROM_EMAIL = 'nuestore@example.com'
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
+else:
+    EMAIL_BACKEND =  'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_POST = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FORM_EMAIL = os.environ.get('EMAIL_HOST_USER')
